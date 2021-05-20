7b9124d85c95d521c8df9251ee0ee208c571257e
Mathieu DUNON <mathieu.dunon@sidexa.blr>
2021-05-20 19:51:28 +0200

    limit offer models

--- models/offer.js
+++ models/offer.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
 const offer = mongoose.model("offer",{
-    product_name: String,
-    product_description: String,
-    product_price: Number,
-    product_details: Array,
-    product_image: { type: mongoose.Schema.Types.Mixed, default: {} },
-    owner: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-  });
+  product_name: {type: String,maxlength:50},
+  product_description:{ type: String,maxlength:500 },
+  product_price: {type: Number, max:100000},
+  product_details: Array,
+  product_image: { type: mongoose.Schema.Types.Mixed, default: {} },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
 
 module.exports = offer;
